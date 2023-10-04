ea18ace3281a674b553018aed5e3ddc08dd3b489
Yash Sharma <139811734+YashSharma412@users.noreply.github.com>
2023-10-04 22:19:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const select = document.getElementById('colorSelect');
+        const button = document.getElementsByTagName('input')[0];
+        // console.log(button);
+        button.addEventListener(('click'), deleteCurrentOption);
+
+        function deleteCurrentOption(){
+            let currentOptValue = select.value;
+            for (let i = 0; i < select.options.length; i++) {
+                const option = select.options[i];
+                if(option.value === currentOptValue){
+                    option.remove();
+                }
+                
+            }
+            console.log(select.value);
+            console.log(select.options[0].value)
+        }
